bb47093f057ef9bf638a847cca2a265855ad7139
Eóin O' Leary <eoin.oleary@evopayments.com>
2022-08-10 14:44:10 +0100

    Update example_data.php

--- examples/example_data.php
+++ examples/example_data.php
@@ -1,15 +1,6 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-// My Deets 800145    u16q9rZ02bW0jqTjxH8t    800145000
-//899996  u16q9rZ02bW0jqTjxH8t 899996000 Hungrrr
-//800443	u16q9rZ02bW0jqTjxH8t	800443000
-//908301    u16q9rZ02bW0jqTjxH8t	908301000 Upco
-//893812    u16q9rZ02bW0jqTjxH8t	893812000
-//915101	u16q9rZ02bW0jqTjxH8t	915101000
-//Morris Builder PROD 915920 47c8uet2VH2UeumYprMN 915920000
-//901214	KCaGwySh4gYjv8XHBETa	901214000
-//UP Deets 400017 w577S336K19ns9q2CC0q 400017000
 $merchantId = "800145";
 $password = "u16q9rZ02bW0jqTjxH8t";
 $brandId = "800145000";
